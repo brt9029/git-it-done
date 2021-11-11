5f8f20a5658761dd54f2b67d7c3b08d73dc3257d
brt9029 <brt9029@gmail.com>
2021-11-10 19:47:57 -0600

    added languages buttons with functionality

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ let userFormEl = document.querySelector("#user-form");
 let nameInputEl = document.querySelector("#username");
 let repoContainerEl = document.querySelector("#repos-container");
 let repoSearchTerm = document.querySelector("#repo-search-term");
+let languageButtonsEl = document.querySelector("#language-buttons")
 
 function getUserRepos(user) {
     // format the github api url
@@ -85,4 +86,31 @@ function displayRepos(repos, searchTerm){
     }
 };
 
+function getFeaturedRepos(language) {
+    let apiURL = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues"
+
+    fetch(apiURL).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    });
+};
+
+function buttonClickHandler(event) {
+    let language = event.target.getAttribute("data-language");
+    
+    if(language) {
+        getFeaturedRepos(language);
+    }
+    
+    // clear old content
+    repoContainerEl.textContent = "";
+};
+
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
